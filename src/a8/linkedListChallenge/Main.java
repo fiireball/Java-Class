9b0767fbef1eecec8d0a2c714dcfec5e84af3986
fiireball <adrian.fiire@gmail.com>
2021-07-04 12:29:51 +0200

    added: linked list challenge skeleton

---
+++ src/a8/linkedListChallenge/Main.java
@@ -0,0 +1,9 @@
+package a8.linkedListChallenge;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/a8/linkedListChallenge/Song.java
@@ -0,0 +1,5 @@
+package a8.linkedListChallenge;
+
+public class Song {
+
+}
